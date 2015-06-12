02f631493a92ba78c6571cb9f072bb32422348c7
Daniel Wallace <danielwallace@gtmanfred.com>
2015-06-12 00:09:01 -0500

    update app2

--- app2.py
+++ app2.py
@@ -4,10 +4,10 @@ from itsdangerous import (TimedJSONWebSignatureSerializer
                                   as Serializer, BadSignature, SignatureExpired)
 from flask import Flask, Response, jsonify, g, session, request
 from flask.ext.login import LoginManager, UserMixin, login_required, current_user
-from flask.ext.httpauth import HTTPBasicAuth
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
+db = SQLAlchemy(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
@@ -16,20 +16,23 @@ login_manager.init_app(app)
 def before_request():
     g.user = current_user
 
-class User(UserMixin):
-    # proxy for a database of users
-    user_database = {"JohnDoe": ("JohnDoe", "John"),
-               "JaneDoe": ("JaneDoe", "Jane")}
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    username = db.Column(db.String(30), primary_key=True)
+    password = db.Column(db.String(128))
+    
 
     def __init__(self, username, password):
-        self.id = username
+        self.username = username
         self.password = password
 
     def generate_auth_token(self, expiration = 600):
         s = Serializer(app.config['SECRET_KEY'], expires_in = expiration)
-        return s.dumps({ 'id': self.id })
+        return s.dumps({ 'username': self.username})
 
-    def verify_auth_token(token):
+    @classmethod
+    def verify_auth_token(cls, token):
         s = Serializer(app.config['SECRET_KEY'])
         try:
             data = s.loads(token)
@@ -37,20 +40,17 @@ class User(UserMixin):
             return None # valid token, but expired
         except BadSignature:
             return None # invalid token
-        user = User.get(data['id'])
-        return user
+        user = User.get(data['username'])
+        return User(user[0], user[1])
 
     @classmethod
-    def get(cls,id):
-        return cls.user_database.get(id)
+    def get(cls, username):
+        return cls.user_database.get(username)
 
 
 @login_manager.request_loader
 def load_user(request):
     token = request.headers.get('X-Auth-Token')
-    if token is None:
-        token = request.args.get('token')
-
     if not token:
         json_dict = request.get_json(force=True)
         if not json_dict:
@@ -62,20 +62,13 @@ def load_user(request):
             user = User(user_entry[0], user_entry[1])
             if user.password == password:
                 return user
+    else:
+        return User.verify_auth_token(token)
+
     return None
 
-@auth.verify_password
-def verify_token(username, password):
-    token = request.headers.get('X-Auth-Token')
-    user = User.verify_auth_token(token)
-    if not user:
-        return False
-    g.user = user
-    return True
-
-
 @app.route("/api/tokens", methods=["POST"])
-@auth.login_required
+@login_required
 def get_api_token():
     token = g.user.generate_auth_token()
     return jsonify({"token": token.decode('utf-8')})
@@ -87,11 +80,13 @@ def index():
 
 
 @app.route("/protected/",methods=["GET", "POST"])
-@auth.login_required
+@login_required
 def protected():
     return Response(response="Hello Protected World!", status=200)
 
 
 if __name__ == '__main__':
     app.config["SECRET_KEY"] = "ITSASECRET"
-    app.run(port=5000,debug=True)
+    app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://localhost/catpics"
+    db.create_all()
+    app.run(port=5000, debug=True)
