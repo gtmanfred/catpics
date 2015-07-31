062201e717ed0afff4a2eba37d1efd4f18633648
Daniel Wallace <danielwallace@gtmanfred.com>
2015-07-31 12:07:24 -0500

    increase upload size to 50M

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 SQLALCHEMY_DATABASE_URI = "postgresql://localhost/catpics"
 SECRET_KEY = "D6E08820-90AF-44F7-9DAD-AA555D1BB9AC"
-DEBUG = True
+MAX_CONTENT_LENGTH = 50 * 1024 * 1024
 
 UPLOAD_FOLDER = '~/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
